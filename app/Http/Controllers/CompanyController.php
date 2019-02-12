a725d13dc157138cd9c62579b024feef3db6acb2
sahib1907 <sfermanli@swgh.az>
2019-02-12 19:06:03 +0400

    error resived

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
-class CompanyController extends Controller
+class CompanyController extends HomeController
 {
     //show
     public function get_companies() {
